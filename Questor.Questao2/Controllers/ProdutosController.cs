4ee773969e012edb9908554d540eb3fe23ce619b
Rodrigo Ribeiro <rlr.para@hotmail.com>
2020-05-11 17:12:17 -0300

    Select com lambda

--- Questor.Questao2/Controllers/ProdutosController.cs
+++ Questor.Questao2/Controllers/ProdutosController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 using Questor.Questao2.Library.Contexto;
+using Questor.Questao2.Library.Domain.Entities;
 
 namespace Questor.Questao2.Controllers
 {
@@ -17,7 +19,30 @@ namespace Questor.Questao2.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            return View(await _questorContext.Produtos.ToListAsync());
+            var prod = await _questorContext.Produtos.ToListAsync();
+            var cli = await _questorContext.Clientes.ToListAsync();
+            var ped = await _questorContext.Pedidos.ToListAsync();
+            var pedItem = await _questorContext.PedidoItens.ToListAsync();
+
+            var result = prod
+                .Join(pedItem,
+                    produto => produto.ProdutoId,
+                    pedidoitem => pedidoitem.ProdutoId,
+                    (produto, pedidoitem) => new { prod = produto, pedItem = pedidoitem })
+                .Join(ped,
+                    pedidoitem => pedidoitem.pedItem.ProdutoId,
+                    pedido => pedido.PedidoId,
+                    (pedidoitem, pedido) => new { pedItem = pedidoitem, ped = pedido })
+                .Join(cli,
+                    pedido => pedido.ped.ClienteId,
+                    cliente => cliente.ClienteId,
+                    (pedido, cliente) => new { ped = pedido, cli = cliente })
+                .Select(x => new
+                {
+                    x.
+                });
+
+            return View();
         }
     }
-}
+};
